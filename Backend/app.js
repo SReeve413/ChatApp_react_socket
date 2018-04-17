2c277abb227ff9db1f825d686b2d54734ef1682a
Shane Reeve <Shanereeve@gmail.com>
2018-04-17 12:08:29 -0600

    first commit

---
+++ Backend/app.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const socket = require('socket.io')
+
+const app = express()
+
+server = app.listen(5000, function(){
+
+    console.log(`server is running on port 5000`)
+
+})
+
+
+io = socket(server)
+
+io.on(`connection`, (socket) => {
+    console.log(socket.id)
+
+    socket.on(`SEND_MESSAGE`, function (data) {
+        io.emit(`RECEIVE_MESSAGE`, data)
+    })
+})
---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Chat from "./Chat"
+
+class App extends Component {
+  render() {
+    return (
+        <React.Fragment>
+          <Chat/>
+        </React.Fragment>
+    );
+  }
+}
+
+export default App;
